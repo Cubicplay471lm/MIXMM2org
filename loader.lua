af8aadbd9f2cca6b5c2e08b36f36ac2dd5f6351d
Cubicplay471lm <0299860@gmail.com>
2026-07-07 18:38:50 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -10,5 +10,6 @@ elseif gameId == 5555555555 then
     
     
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Cubicplay471lm/MIXMM2org/refs/heads/main/mm2.lua?token=GHSAT0AAAAAAEBJJ3VSBZAQ5CFJ2UWWNVEW2SNDTZQ"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Cubicplay471lm/MIXMM2org/refs/heads/main/mm2.lua?token=GHSAT0AAAAAAEBJJ3VSBZAQ5CFJ2UWWNVEW2SNDTZQ"))()
+	print("загрузка")
 end
